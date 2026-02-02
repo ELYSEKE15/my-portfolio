38801a20358bae6d86c4f65891ce292858b13a4b
ELYSEKE15 <kevinelyse@gmail.com>
2026-02-02 09:42:26 +0200

    MY_project

---
+++ script.js
@@ -0,0 +1,10 @@
+// const icons = document.querySelectorAll(".tech");
+
+// setInterval(() => {
+//   icons.forEach(icon => {
+//     icon.style.transform = `scale(${1 + Math.random() * 0.4})`;
+//     setTimeout(() => {
+//       icon.style.transform = "scale(1)";
+//     }, 300);
+//   });
+// }, 2800);
